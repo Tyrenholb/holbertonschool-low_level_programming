7d3f480326414f63533c05720f920e406b3cae65
Tyren Howarth <14807@holbertonstudents.com>
2026-07-07 13:35:39 +1000

    Completed task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * prints the alphabet in lowercase followed by a new line.
+ * can only use putchar twice
+ *
+ * return: always 0
+ */
+	int main(void)
+	{
+		char ch;
+
+		for (ch ='a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+
+		return (0);
+	}
