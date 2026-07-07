020070555eb998c86856b47ffe4152c13a51953a
Tyren Howarth <14807@holbertonstudents.com>
2026-07-07 14:17:48 +1000

    Completed  task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase and skips the letters e & q
- * 
- * Return: Always 0 (Success)
- */
+* main - Prints the alphabet in lowercase and skips the letters e & q
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'q' && ch != 'e') 
+		if (ch != 'q' && ch != 'e')
 		{
 			putchar(ch);
 		}
