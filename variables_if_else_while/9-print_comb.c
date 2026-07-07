1c223c71ad1813d2ef7d1604332b5d7ef9389736
Tyren Howarth <14807@holbertonstudents.com>
2026-07-07 15:50:39 +1000

    Completed task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers with commas.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nu;
+
+	for (nu = '0'; nu <= '9'; nu++)
+	{
+		putchar(nu);
+		if (nu < '9')
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return(0);
+}
