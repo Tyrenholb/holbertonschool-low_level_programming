06135af927721b2f314197e60bdd1d6d3b30b637
Tyren Howarth <14807@holbertonstudents.com>
2026-07-07 15:32:37 +1000

    Completed task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,22 +1,22 @@
-# include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Prints all the numbers of base 16 in lowercase, followed by a new line.
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch;
 	char nu;
 
-	for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
-	}
 	for (nu = '0'; nu <= '9'; nu++)
 	{
 		putchar(nu);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
 	}
 	putchar('\n');
 
