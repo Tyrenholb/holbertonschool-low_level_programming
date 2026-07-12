e70e5f4b4e4e594477abdb9387751428cc422744
Tyren Howarth <14807@holbertonstudents.com>
2026-07-12 17:29:19 +1000

    Completed task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 1 to 100 with FizzBuzz rules
- *
- * Return: Always 0
- */
+* main - Prints the numbers from 1 to 100 with FizzBuzz rules
+*
+* Return: Always 0
+*/
 int main(void)
 {
 	int n;
