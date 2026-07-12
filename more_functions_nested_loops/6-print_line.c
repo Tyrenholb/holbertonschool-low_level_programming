fd2107174bf1e4998e25eaf894c46acaaef727c4
Tyren Howarth <14807@holbertonstudents.com>
2026-07-12 17:10:34 +1000

    Completed task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: Number of underscores to print
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		_putchar('_');
+		count++;
+	}
+	_putchar('\n');
+}
