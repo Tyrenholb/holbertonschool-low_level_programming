c259193b68e2641153b62c4349e040972d911389
Tyren Howarth <14807@holbertonstudents.com>
2026-07-14 13:29:40 +1000

    Completed task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s: pointer to the string
+*
+* Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
