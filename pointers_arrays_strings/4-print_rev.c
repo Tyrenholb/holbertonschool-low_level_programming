e77c040b039e9603a00586c7f9a565501dfa322b
Tyren Howarth <14807@holbertonstudents.com>
2026-07-14 14:20:59 +1000

    Completed task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string in reverse
+* @s: pointer to the string
+*/
+void print_rev(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	length--;
+
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+
+	_putchar('\n');
+}
