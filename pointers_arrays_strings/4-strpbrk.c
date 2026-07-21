8407fe90c2f2e548a494699e94cf9a2c8240474e
Tyren Howarth <14807@holbertonstudents.com>
2026-07-21 13:47:41 +1000

    Completed task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: bytes to match
+ *
+ * Return: pointer to first matching byte, or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return (0);
+}
