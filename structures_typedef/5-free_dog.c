802f19207507e3b03030a90dd513834d46a72733
Tyren Howarth <14807@holbertonstudents.com>
2026-07-26 18:01:54 +1000

    Completed task 4

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees a dog structure.
+ * @d: Pointer to the dog.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
