169dd6ec94922408adb342410c638b442de002c1
Tyren Howarth <14807@holbertonstudents.com>
2026-08-03 12:55:35 +1000

    Completed task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *main - Multiplies two numbers
+ *@argc: Number of command-line arguments
+ *@argv: Array of command-line argument strings
+ *
+ *Return: 0 on success, 1 on error
+ */
+
 int main(int argc, char *argv[])
 {
 	int first;
