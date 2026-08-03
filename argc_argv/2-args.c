3c75dcbb2ec32e371819241b18ff17bb4c3a9dcb
Tyren Howarth <14807@holbertonstudents.com>
2026-08-03 12:51:59 +1000

    Completed task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Prints all arguments it receives
+ *@argc: Number of command line arguments
+ *@argv: Array of command line argument strings
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
