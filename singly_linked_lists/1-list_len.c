ce3ac9e716f300c6f40b1e0d774ae767542aba9e
Tyren Howarth <14807@holbertonstudents.com>
2026-08-09 17:59:38 +1000

    Completed task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - Returns the number of elements in a linked list
+ * @h: Pointer to the first node
+ *
+ * Return: The elements in the linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return(count);
+}
