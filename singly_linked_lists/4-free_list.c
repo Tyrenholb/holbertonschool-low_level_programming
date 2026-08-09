4fcf456f807eb246c2729e5c068107daeb80f720
Tyren Howarth <14807@holbertonstudents.com>
2026-08-09 21:06:16 +1000

    Completed task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the first node
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
