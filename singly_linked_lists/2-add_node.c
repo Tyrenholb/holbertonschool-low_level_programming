1e5a119b11c0be8c41017ea7dba451e564dbd1cb
Tyren Howarth <14807@holbertonstudents.com>
2026-08-09 19:25:41 +1000

    Completed task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
@@ -9,29 +9,28 @@
  *
  * Return: Address of the new node, or NULL on failure
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	unsigned int i = 0;
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		return(NULL);
-	}
+		return (NULL);
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return(NULL);
+		return (NULL);
 	}
-	unsigned int i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	new_node->len = i;
-	new_node->next = (*head);
 
-	(*head) = new_node;
+	while (str[i] != '\0')
+		i++;
+
+	new_node->len = i;
+	new_node->next = *head;
+	*head = new_node;
 
 	return (new_node);
 }
