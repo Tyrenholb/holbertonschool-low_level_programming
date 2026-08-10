e18dfa31af1fe44831792eed0a6fa2f4c29b6566
Tyren Howarth <14807@holbertonstudents.com>
2026-08-10 17:09:33 +1000

    Completed task 2

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -9,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -21,5 +20,8 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+
 
 #endif
---
+++ doubly_linked_lists/test_size.c
@@ -0,0 +1,2 @@
+#include <stddef.h>
+int main(void) { size_t x = 0; return (x); }
