20b5ec01cb8b2bc7a092c4f8048e27ffc9874af4
Tyren Howarth <14807@holbertonstudents.com>
2026-08-10 16:37:52 +1000

    Completed task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -17,9 +17,9 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		if (h == NULL)
-			printf("[0] (nil)\n");
+			printf("0 (nil)\n");
 		else
-			printf("[%i] \n", h->n);
+			printf("%i \n", h->n);
 
 		count++;
 		h = h->next;
