81ba65ced5c777cd001e6aab78b3ac43d25b76cc
Tyren Howarth <14807@holbertonstudents.com>
2026-08-18 14:20:29 +1000

    Completed task 6

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,13 +9,12 @@
  */
 int sqrt_helper(int n, int guess)
 {
-	if (guess * guess == n)
-		return (guess);
+if (guess * guess == n)
+return (guess);
+if (guess * guess > n)
+return (-1);
 
-	if (guess * guess > n)
-		return (-1);
-
-	return (sqrt_helper(n, guess + 1));
+return (sqrt_helper(n, guess + 1));
 }
 
 /**
@@ -26,8 +25,8 @@ int sqrt_helper(int n, int guess)
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
+if (n < 0)
+return (-1);
 
-	return (sqrt_helper(n, 0));
+return (sqrt_helper(n, 0));
 }
