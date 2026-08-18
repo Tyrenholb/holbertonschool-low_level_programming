826c4a6a6b24b5deafea12c4e0734c842475ef74
Tyren Howarth <14807@holbertonstudents.com>
2026-08-18 10:45:22 +1000

    Completed task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-/*
+/**
 * _puts_recursion - prints a string followed by a new line
-* @s: pointer to the string to be printed
+* @s: string to print
 *
 * Return: void
 */
