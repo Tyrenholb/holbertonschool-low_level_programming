21284530baa486faf873defade4940eb8516b13d
Tyren Howarth <14807@holbertonstudents.com>
2026-08-18 11:40:49 +1000

    Completed task 3

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,7 +5,7 @@
  * _strlen_recursion - returns the length of a string
  * @s: string to print
  *
- * return: int
+ * return: the total number of chars in the string.
  */
 int _strlen_recursion(char *s)
 {
