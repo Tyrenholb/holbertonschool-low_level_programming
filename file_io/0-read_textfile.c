f0649038dff0e75c29b3ec93cc98d9516fad20dd
Tyren Howarth <14807@holbertonstudents.com>
2026-08-21 18:32:03 +1000

    Completed task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * read_textfile - reads a text file and prints it to stdout
@@ -15,42 +13,42 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char *buffer;
-    ssize_t bytes_written;
-    ssize_t bytes_read;
+	int fd;
+	char *buffer;
+	ssize_t bytes_written;
+	ssize_t bytes_read;
 
-    if (filename == NULL)
-        return (0);
-    
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	if (filename == NULL)
+	return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        free(buffer);
-        return (0);
-    }
-    bytes_read = read (fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	return (0);
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+{
+	free(buffer);
+	return (0);
+}
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+{
+	free(buffer);
+	close(fd);
+	return (0);
+}
 
-    free(buffer);
-    close(fd);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+{
+	free(buffer);
+	close(fd);
+	return (0);
+}
 
-    return (bytes_written);
+	free(buffer);
+	close(fd);
+
+	return (bytes_written);
 }
